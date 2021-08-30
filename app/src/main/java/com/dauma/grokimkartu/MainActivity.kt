cfe02289644ec5fbfab6b707f6f95c688eddfebd
daumantasp <daumantasp@gmail.com>
2021-08-30 20:00:57 +0300

    Add initial project files

---
+++ app/src/main/java/com/dauma/grokimkartu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dauma.grokimkartu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
