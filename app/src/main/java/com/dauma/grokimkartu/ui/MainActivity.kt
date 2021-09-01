2b72bff9648ea497b5b5d46838edafb1b82293d6
daumantasp <daumantasp@gmail.com>
2021-09-01 18:31:08 +0300

    Create project structure

--- app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dauma.grokimkartu
+package com.dauma.grokimkartu.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dauma.grokimkartu.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
