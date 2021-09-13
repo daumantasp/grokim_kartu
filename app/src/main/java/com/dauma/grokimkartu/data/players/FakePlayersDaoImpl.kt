3b979a99af327a7a5232c55a644550b98f8251c3
daumantasp <daumantasp@gmail.com>
2021-09-13 20:13:54 +0300

    Add FakePlayersDao

---
+++ app/src/main/java/com/dauma/grokimkartu/data/players/FakePlayersDaoImpl.kt
@@ -0,0 +1,27 @@
+package com.dauma.grokimkartu.data.players
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.dauma.grokimkartu.models.Player
+
+class FakePlayersDaoImpl : PlayersDao {
+    private val playerList = mutableListOf<Player>()
+    private val players = MutableLiveData<List<Player>>()
+
+    init {
+        playerList.addAll(listOf(
+            Player(1, "Daumantas", "Gitara"),
+            Player(2, "Petras", "Armonika")
+        ))
+        players.value = playerList
+    }
+
+    override fun addPlayer(player: Player) {
+        playerList.add(player)
+        players.value = playerList
+    }
+
+    override fun getPlayers(): LiveData<List<Player>> {
+        return players as LiveData<List<Player>>
+    }
+}
---
+++ app/src/main/java/com/dauma/grokimkartu/data/players/PlayersDao.kt
@@ -0,0 +1,9 @@
+package com.dauma.grokimkartu.data.players
+
+import androidx.lifecycle.LiveData
+import com.dauma.grokimkartu.models.Player
+
+interface PlayersDao {
+    fun getPlayers() : LiveData<List<Player>>
+    fun addPlayer(player: Player)
+}
