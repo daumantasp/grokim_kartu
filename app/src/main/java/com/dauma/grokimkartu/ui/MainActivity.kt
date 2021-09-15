fa7f844b1e5e2e849ad5626d05b5a74aa08487c9
daumantasp <daumantasp@gmail.com>
2021-09-15 19:30:40 +0300

    Do not show bottom navigation view in login fragment

--- app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dauma.grokimkartu.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -8,6 +10,12 @@ import com.dauma.grokimkartu.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var bottomNavigationView: BottomNavigationView? = null
+
+    companion object {
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,9 +23,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeBottomNavigation() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView!!.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginFragment -> showBottomNavigation(false)
+                else -> showBottomNavigation(true)
+            }
+        }
+    }
+
+    private fun showBottomNavigation(show: Boolean) {
+        if (bottomNavigationView == null) {
+            Log.d(TAG, "ERROR: bottomNavigationView is not initialized")
+        }
+
+        bottomNavigationView!!.visibility = if (show == true) View.VISIBLE else View.GONE
     }
 }
