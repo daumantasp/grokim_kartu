2fbaae036975ac24dbad5a2e3ec5889747aa70d0
daumantasp <daumantasp@gmail.com>
2021-10-13 20:31:11 +0300

    Use databinding in registration fragment

--- app/src/main/java/com/dauma/grokimkartu/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/registration/RegistrationFragment.kt
@@ -8,10 +8,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.dauma.grokimkartu.R
+import com.dauma.grokimkartu.databinding.FragmentRegistrationBinding
 import com.dauma.grokimkartu.viewmodels.registration.RegistrationViewModelImpl
 import dagger.hilt.android.AndroidEntryPoint
 import com.google.firebase.auth.FirebaseAuth
@@ -20,72 +22,37 @@ import com.google.firebase.auth.FirebaseAuth
 class RegistrationFragment : Fragment() {
     private val registrationViewModel by viewModels<RegistrationViewModelImpl>()
     private var mAuth: FirebaseAuth? = null
-    private var closeImageButton: ImageButton? = null
-    private var nameEditText: EditText? = null
-    private var emailEditText: EditText? = null
-    private var passwordEditText: EditText? = null
-    private var registerButton: Button? = null
+
+    private var _binding: FragmentRegistrationBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_registration, container, false)
-        closeImageButton = rootView.findViewById(R.id.closeImageButton)
-        nameEditText = rootView.findViewById(R.id.nameEditText)
-        emailEditText = rootView.findViewById(R.id.emailEditText)
-        passwordEditText = rootView.findViewById(R.id.passwordEditText)
-        registerButton = rootView.findViewById(R.id.registerButton)
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        binding.model = registrationViewModel
+        val view = binding.root
 
         mAuth = FirebaseAuth.getInstance();
 
         // TODO: Implement it in MVVM pattern
-        closeImageButton!!.setOnClickListener {
+        binding.closeImageButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_registrationFragment_to_loginFragment)
         }
 
-        registerButton!!.setOnClickListener {
-            registerUser()
+        registrationViewModel.getRegistrationForm().getFormFields().observe(viewLifecycleOwner) {
+            registrationViewModel.createUser(it.get(0), it.get(1), it.get(2))
         }
 
-        return rootView
+        return view
     }
 
-    private fun registerUser() {
-        val name = nameEditText?.text.toString().trim()
-        val email = emailEditText?.text.toString().trim()
-        val password = passwordEditText?.text.toString().trim()
-
-        if (name.isEmpty()) {
-            nameEditText?.error = "Name is required!"
-            nameEditText?.requestFocus()
-            return
-        }
-
-        if (email.isEmpty()) {
-            emailEditText?.error = "Email is required!"
-            emailEditText?.requestFocus()
-            return
-        }
-
-        if (Patterns.EMAIL_ADDRESS.matcher(email).matches() == false) {
-            emailEditText?.error = "Please provie valid email!"
-            emailEditText?.requestFocus()
-            return
-        }
-
-        if (password.isEmpty()) {
-            passwordEditText?.error = "Password is required!"
-            passwordEditText?.requestFocus()
-            return
-        }
-
-        if (password.length < 6) {
-            passwordEditText?.error = "Min password length should be 6 characters!"
-            passwordEditText?.requestFocus()
-            return
-        }
-
-        registrationViewModel.createUser(name, email, password)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
