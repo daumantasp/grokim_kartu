0742f4085bb3fcb3744ae1b1e74842030498d424
daumantasp <daumantasp@gmail.com>
2021-10-18 19:32:15 +0300

    Add loginUser to UserRepositoryImpl

--- app/src/main/java/com/dauma/grokimkartu/repositories/users/UsersRepository.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/users/UsersRepository.kt
@@ -1,8 +1,10 @@
 package com.dauma.grokimkartu.repositories.users
 
+import com.dauma.grokimkartu.models.users.LoginUser
 import com.dauma.grokimkartu.models.users.RegistrationUser
 import com.dauma.grokimkartu.models.users.User
 
 interface UsersRepository {
     fun registerUser(user: RegistrationUser, onComplete: (Boolean, User?) -> Unit)
+    fun loginUser(user: LoginUser, onComplete: (Boolean) -> Unit)
 }
--- app/src/main/java/com/dauma/grokimkartu/repositories/users/UsersRepositoryImpl.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/users/UsersRepositoryImpl.kt
@@ -1,11 +1,16 @@
 package com.dauma.grokimkartu.repositories.users
 
 import com.dauma.grokimkartu.data.users.UsersDao
+import com.dauma.grokimkartu.models.users.LoginUser
 import com.dauma.grokimkartu.models.users.RegistrationUser
 import com.dauma.grokimkartu.models.users.User
 
 class UsersRepositoryImpl(private val usersDao: UsersDao) : UsersRepository {
     override fun registerUser(user: RegistrationUser, onComplete: (Boolean, User?) -> Unit) {
         usersDao.registerUser(user, onComplete)
+    }
+
+    override fun loginUser(user: LoginUser, onComplete: (Boolean) -> Unit) {
+        usersDao.loginUser(user, onComplete)
     }
 }
