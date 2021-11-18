f4b401c8fe9e40ce61caba76c1df2600e074bbff
daumantasp <daumantasp@gmail.com>
2021-11-18 19:21:23 +0200

    Fix bottom navigation do not navigate to home/players fragment

--- app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.dauma.grokimkartu.R
