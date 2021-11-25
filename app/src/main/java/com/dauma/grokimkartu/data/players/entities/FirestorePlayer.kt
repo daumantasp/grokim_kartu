29ac22842359a4e01af17abae37bd5d0185b7cfc
daumantasp <daumantasp@gmail.com>
2021-11-25 20:25:31 +0200

    Add FirestorePlayer

--- app/src/main/java/com/dauma/grokimkartu/data/players/entities/FirestorePlayer.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/players/entities/FirestorePlayer.kt
@@ -1,9 +1,17 @@
 package com.dauma.grokimkartu.data.players.entities
 
-data class FirestorePlayer(
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ServerTimestamp
+
+class FirestorePlayer(
     val userId: String,
+    @ServerTimestamp
+    val registrationDate: Timestamp,
     val visible: Boolean,
     val name: String,
     val instrument: String
 ) {
+    // Empty constructor is a must for Firestore
+    constructor() : this("", Timestamp.now(), false, "", "")
 }
--- app/src/main/java/com/dauma/grokimkartu/data/players/entities/Player.kt
+++
@@ -1,8 +0,0 @@
-package com.dauma.grokimkartu.data.players.entities
-
-data class Player(
-    var id: Long,
-    var name: String,
-    var instrument: String
-) {
-}
