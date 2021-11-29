aafa476135583c1eece02e53540ae499cc504c54
daumantasp <daumantasp@gmail.com>
2021-11-29 18:34:35 +0200

    Add settings view model and bind it to the fragment

---
+++ app/src/main/java/com/dauma/grokimkartu/viewmodels/main/SettingsViewModel.kt
@@ -0,0 +1,14 @@
+package com.dauma.grokimkartu.viewmodels.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor(
+
+) : ViewModel() {
+    companion object {
+        private val TAG = "SettingsViewModel"
+    }
+}
