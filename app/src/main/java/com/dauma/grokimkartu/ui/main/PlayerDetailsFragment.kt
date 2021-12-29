2159649ffe14f423208201da53a0a9c77bc4a3ee
daumantasp <daumantasp@gmail.com>
2021-12-29 20:59:34 +0200

    Add PlayerDetailsViewModel

--- app/src/main/java/com/dauma/grokimkartu/ui/main/PlayerDetailsFragment.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/main/PlayerDetailsFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.dauma.grokimkartu.R
+import com.dauma.grokimkartu.databinding.FragmentPlayerDetailsBinding
+import com.dauma.grokimkartu.viewmodels.main.PlayerDetailsViewModel
 
 class PlayerDetailsFragment : Fragment() {
+    private val playerDetailsViewModel by viewModels<PlayerDetailsViewModel>()
+
+    private var _binding: FragmentPlayerDetailsBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
