e4d164249d00d85b4a0e5f587b3bcf78e3da8b58
daumantasp <daumantasp@gmail.com>
2022-01-25 19:13:15 +0200

    Little spinner animation improvement with tY

--- app/src/main/java/com/dauma/grokimkartu/ui/viewelements/SpinnerViewElement.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/viewelements/SpinnerViewElement.kt
@@ -13,6 +13,7 @@ import com.dauma.grokimkartu.R
 //https://medium.com/@douglas.iacovelli/the-beauty-of-custom-views-and-how-to-do-it-79c7d78e2088
 class SpinnerViewElement(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     private val animatorSet: AnimatorSet = AnimatorSet()
+    private val spinnerContainerLinearLayout: LinearLayout
     private var spinnerCircles: MutableList<ImageView> = mutableListOf()
     private var isAnimationRunning: Boolean = false
 
@@ -22,6 +23,7 @@ class SpinnerViewElement(context: Context, attrs: AttributeSet) : LinearLayout(c
 
     init {
         inflate(context, R.layout.element_spinner, this)
+        spinnerContainerLinearLayout = findViewById(R.id.spinnerContainerLinearLayout)
         spinnerCircles.add(findViewById(R.id.spinner_circle_1))
         spinnerCircles.add(findViewById(R.id.spinner_circle_2))
         spinnerCircles.add(findViewById(R.id.spinner_circle_3))
@@ -29,6 +31,9 @@ class SpinnerViewElement(context: Context, attrs: AttributeSet) : LinearLayout(c
 
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.SpinnerViewElement)
         val unitDimensions = attributes.getDimension(R.styleable.SpinnerViewElement_unitDimensions, 18f)
+        spinnerContainerLinearLayout.clipChildren = false
+        spinnerContainerLinearLayout.clipToPadding = false
+        spinnerContainerLinearLayout.setPadding(unitDimensions.toInt(), unitDimensions.toInt(), unitDimensions.toInt(), unitDimensions.toInt())
         for (i in 0 until spinnerCircles.count()) {
             spinnerCircles[i].layoutParams.width = unitDimensions.toInt()
             spinnerCircles[i].layoutParams.height = unitDimensions.toInt()
@@ -42,18 +47,26 @@ class SpinnerViewElement(context: Context, attrs: AttributeSet) : LinearLayout(c
             animatorSet.duration = spinnerCircles.count() * CIRCLE_ANIMATION_DURATION
             var animatorSetBuilder: AnimatorSet.Builder? = null
             for (i in 0 until spinnerCircles.count()) {
-                val scaleAnimator = ValueAnimator.ofFloat(1.0f, 0.1f, 1.0f)
+                val tY = (spinnerCircles[i].layoutParams.height / -2.0).toFloat()
+                val tYAnimator = ValueAnimator.ofFloat(0f, tY, 0f)
+                tYAnimator.addUpdateListener {
+                    val value = it.animatedValue as Float
+                    spinnerCircles[i].translationY = value
+                }
+                val scaleAnimator = ValueAnimator.ofFloat(1.0f, 0.3f, 1.0f)
                 scaleAnimator.addUpdateListener {
                     val value = it.animatedValue as Float
                     spinnerCircles[i].scaleX = value
                     spinnerCircles[i].scaleY = value
                 }
+                tYAnimator.startDelay = i * CIRCLE_ANIMATION_DURATION
+                tYAnimator.repeatCount = Animation.INFINITE
                 scaleAnimator.startDelay = i * CIRCLE_ANIMATION_DURATION
                 scaleAnimator.repeatCount = Animation.INFINITE
                 if (i == 0) {
-                    animatorSetBuilder = animatorSet.play(scaleAnimator)
+                    animatorSetBuilder = animatorSet.play(tYAnimator).with(scaleAnimator)
                 } else {
-                    animatorSetBuilder!!.with(scaleAnimator)
+                    animatorSetBuilder!!.with(tYAnimator).with(scaleAnimator)
                 }
             }
 
@@ -64,4 +77,5 @@ class SpinnerViewElement(context: Context, attrs: AttributeSet) : LinearLayout(c
             isAnimationRunning = false
         }
     }
+    // https://www.raywenderlich.com/2785491-android-animation-tutorial-with-kotlin
 }
