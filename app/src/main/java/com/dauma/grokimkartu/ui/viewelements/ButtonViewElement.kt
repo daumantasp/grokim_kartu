d381f7da8b0fadb953e27cdf31ed9d0098ac3079
daumantasp <daumantasp@gmail.com>
2022-02-07 20:02:29 +0200

    Add secondary button style (not finished but at current state its okay)

--- app/src/main/java/com/dauma/grokimkartu/ui/viewelements/ButtonViewElement.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/viewelements/ButtonViewElement.kt
@@ -1,7 +1,12 @@
 package com.dauma.grokimkartu.ui.viewelements
 
 import android.content.Context
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.PorterDuff
+import android.os.Build
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.widget.Button
 import android.widget.FrameLayout
 import com.dauma.grokimkartu.R
@@ -20,7 +25,9 @@ class ButtonViewElement(context: Context, attrs: AttributeSet) : FrameLayout(con
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.ButtonViewElement)
         text = attributes.getString(R.styleable.ButtonViewElement_text) ?: ""
         button.text = text
+        val type = attributes.getInt(R.styleable.ButtonViewElement_type, 0)
         attributes.recycle()
+        setType(type)
     }
 
     override fun setEnabled(isEnabled: Boolean) {
@@ -37,5 +44,17 @@ class ButtonViewElement(context: Context, attrs: AttributeSet) : FrameLayout(con
     fun showAnimation(show: Boolean) {
         button.text = if (show) "" else text
         spinner.showAnimation(show)
+    }
+
+    private fun setType(type: Int) {
+        if (type == 0) {
+            // Default
+        } else if (type == 1) {
+            // Secondary
+            val typedValue = TypedValue()
+            context.theme.resolveAttribute(R.attr.secondaryButtonTextColor, typedValue, true)
+            button.setBackgroundResource(R.drawable.secondary_button_ripple)
+            button.setTextColor(typedValue.data)
+        }
     }
 }
