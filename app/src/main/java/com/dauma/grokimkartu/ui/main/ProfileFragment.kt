0e3b63f64be7ffdaf3ed81798e0fb719ee1881ed
daumantasp <daumantasp@gmail.com>
2022-02-27 17:07:22 +0200

    Set dialogManager to null onDestroy (possible memory leak fix)

--- app/src/main/java/com/dauma/grokimkartu/ui/main/ProfileFragment.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/main/ProfileFragment.kt
@@ -89,6 +89,7 @@ class ProfileFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        dialogsManager = null
     }
 
     private fun setupObservers() {
