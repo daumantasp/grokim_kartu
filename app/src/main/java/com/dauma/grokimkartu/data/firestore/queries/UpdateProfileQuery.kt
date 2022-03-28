173bdcba54d9e539ffb0009173e83b5ee717c9a9
daumantasp <daumantasp@gmail.com>
2022-03-28 09:35:47 +0300

    Throw exception if input is not provided in UpdateProfileQuery

--- app/src/main/java/com/dauma/grokimkartu/data/firestore/queries/UpdateProfileQuery.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/firestore/queries/UpdateProfileQuery.kt
@@ -7,30 +7,34 @@ class UpdateProfileQuery(firebaseFirestore: FirebaseFirestore)
     : FirestoreInputQuery<Nothing, FirestoreProfile>(firebaseFirestore) {
     override fun execute() {
         if (id != null) {
-            val valuesToSet: HashMap<String, Any> = hashMapOf()
-            if (input?.name != null) {
-                valuesToSet["name"] = input?.name!!
-            }
-            if (input?.instrument != null) {
-                valuesToSet["instrument"] = input?.instrument!!
-            }
-            if (input?.description != null) {
-                valuesToSet["description"] = input?.description!!
-            }
-            if (input?.city != null) {
-                valuesToSet["city"] = input?.city!!
-            }
+            if (input != null) {
+                val valuesToSet: HashMap<String, Any> = hashMapOf()
+                if (input?.name != null) {
+                    valuesToSet["name"] = input?.name!!
+                }
+                if (input?.instrument != null) {
+                    valuesToSet["instrument"] = input?.instrument!!
+                }
+                if (input?.description != null) {
+                    valuesToSet["description"] = input?.description!!
+                }
+                if (input?.city != null) {
+                    valuesToSet["city"] = input?.city!!
+                }
 
-            firebaseFirestore
-                .collection(usersCollection)
-                .document(id!!)
-                .update("profile", valuesToSet)
-                .addOnSuccessListener { _ ->
-                    onSuccess(null)
-                }
-                .addOnFailureListener { e ->
-                    onFailure(e)
-                }
+                firebaseFirestore
+                    .collection(usersCollection)
+                    .document(id!!)
+                    .update("profile", valuesToSet)
+                    .addOnSuccessListener { _ ->
+                        onSuccess(null)
+                    }
+                    .addOnFailureListener { e ->
+                        onFailure(e)
+                    }
+            } else {
+                throw Exception("Input is not provided")
+            }
         } else {
             throw Exception("User id is not provided")
         }
