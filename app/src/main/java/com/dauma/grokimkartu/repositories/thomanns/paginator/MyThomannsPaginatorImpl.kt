d45153607dd560a2ce59db898239bcdd517a821b
daumantasp <daumantasp@gmail.com>
2022-12-03 10:26:49 +0200

    Add MyThomannsPaginator

---
+++ app/src/main/java/com/dauma/grokimkartu/repositories/thomanns/paginator/MyThomannsPaginatorImpl.kt
@@ -0,0 +1,23 @@
+package com.dauma.grokimkartu.repositories.thomanns.paginator
+
+import com.dauma.grokimkartu.data.thomanns.ThomannsDao
+import com.dauma.grokimkartu.data.thomanns.entities.ThomannsResponse
+import com.dauma.grokimkartu.repositories.thomanns.ThomannsErrors
+
+class MyThomannsPaginatorImpl(private val thomannsDao: ThomannsDao) : ThomannsPaginator(thomannsDao) {
+    override fun loadNextPage(accessToken: String, onComplete: (ThomannsResponse?, ThomannsErrors?) -> Unit) {
+        if (isLastLoaded() == false) {
+            val nextPage = _pages.count() + 1
+            thomannsDao.myThomanns(nextPage, pageSize, accessToken) { thomannsResponse, thomannsDaoResponseStatus ->
+                if (thomannsDaoResponseStatus.isSuccessful && thomannsResponse != null) {
+                    _pages.add(thomannsResponse)
+                    onComplete(thomannsResponse, null)
+                } else {
+                    onComplete(null, ThomannsErrors.UNKNOWN)
+                }
+            }
+        } else {
+            onComplete(_pages.lastOrNull(), null)
+        }
+    }
+}
