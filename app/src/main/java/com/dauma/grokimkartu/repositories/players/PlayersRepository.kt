03d7d61786773f18ed656f840171ab85916bf486
daumantasp <daumantasp@gmail.com>
2022-12-10 16:52:53 +0200

    Implement cities and instruments methods in players repository

--- app/src/main/java/com/dauma/grokimkartu/repositories/players/PlayersRepository.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/players/PlayersRepository.kt
@@ -1,7 +1,9 @@
 package com.dauma.grokimkartu.repositories.players
 
 import android.graphics.Bitmap
+import com.dauma.grokimkartu.repositories.players.entities.PlayerCity
 import com.dauma.grokimkartu.repositories.players.entities.PlayerDetails
+import com.dauma.grokimkartu.repositories.players.entities.PlayerInstrument
 import com.dauma.grokimkartu.repositories.players.entities.PlayersPage
 
 interface PlayersRepository {
@@ -13,4 +15,8 @@ interface PlayersRepository {
     fun playerPhoto(userId: Int, onComplete: (Bitmap?, PlayersErrors?) -> Unit)
     fun playerIcon(userId: Int, onComplete: (Bitmap?, PlayersErrors?) -> Unit)
     fun reload(onComplete: (PlayersPage?, PlayersErrors?) -> Unit)
+    fun cities(onComplete: (List<PlayerCity>?, PlayersErrors?) -> Unit)
+    fun searchCity(value: String, onComplete: (List<PlayerCity>?, PlayersErrors?) -> Unit)
+    fun instruments(onComplete: (List<PlayerInstrument>?, PlayersErrors?) -> Unit)
+    fun searchInstrument(value: String, onComplete: (List<PlayerInstrument>?, PlayersErrors?) -> Unit)
 }
--- app/src/main/java/com/dauma/grokimkartu/repositories/players/PlayersRepositoryImpl.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/players/PlayersRepositoryImpl.kt
@@ -2,17 +2,19 @@ package com.dauma.grokimkartu.repositories.players
 
 import android.graphics.Bitmap
 import com.dauma.grokimkartu.data.cities.CitiesDao
+import com.dauma.grokimkartu.data.cities.entities.CityResponse
 import com.dauma.grokimkartu.data.instruments.InstrumentsDao
+import com.dauma.grokimkartu.data.instruments.entities.InstrumentResponse
 import com.dauma.grokimkartu.data.players.PlayersDao
 import com.dauma.grokimkartu.data.players.PlayersDaoResponseStatus
 import com.dauma.grokimkartu.data.players.entities.PlayersResponse
 import com.dauma.grokimkartu.general.IconLoader
 import com.dauma.grokimkartu.general.user.User
 import com.dauma.grokimkartu.repositories.auth.LoginListener
-import com.dauma.grokimkartu.repositories.players.entities.Player
-import com.dauma.grokimkartu.repositories.players.entities.PlayerDetails
-import com.dauma.grokimkartu.repositories.players.entities.PlayersPage
+import com.dauma.grokimkartu.repositories.players.entities.*
 import com.dauma.grokimkartu.repositories.players.paginator.PlayersPaginator
+import com.dauma.grokimkartu.repositories.profile.ProfileErrors
+import com.dauma.grokimkartu.repositories.profile.ProfileException
 import com.dauma.grokimkartu.repositories.users.AuthenticationErrors
 
 class PlayersRepositoryImpl(
@@ -142,6 +144,72 @@ class PlayersRepositoryImpl(
         }
     }
 
+    override fun cities(onComplete: (List<PlayerCity>?, PlayersErrors?) -> Unit) {
+        if (user.isUserLoggedIn()) {
+            citiesDao.cities(user.getBearerAccessToken()!!) { citiesResponse, citiesDaoResponseStatus ->
+                if (citiesDaoResponseStatus.isSuccessful && citiesResponse != null) {
+                    val playerCities = citiesResponse.map { cr -> toPlayerCity(cr) }
+                    onComplete(playerCities, null)
+                } else {
+                    onComplete(null, PlayersErrors.UNKNOWN)
+                }
+            }
+        } else {
+            throw ProfileException(ProfileErrors.USER_NOT_LOGGED_IN)
+        }
+    }
+
+    override fun searchCity(
+        value: String,
+        onComplete: (List<PlayerCity>?, PlayersErrors?) -> Unit
+    ) {
+        if (user.isUserLoggedIn()) {
+            citiesDao.search(value, user.getBearerAccessToken()!!) { citiesResponse, citiesDaoResponseStatus ->
+                if (citiesDaoResponseStatus.isSuccessful && citiesResponse != null) {
+                    val playerCities = citiesResponse.map { cr -> toPlayerCity(cr) }
+                    onComplete(playerCities, null)
+                } else {
+                    onComplete(null, PlayersErrors.UNKNOWN)
+                }
+            }
+        } else {
+            throw ProfileException(ProfileErrors.USER_NOT_LOGGED_IN)
+        }
+    }
+
+    override fun instruments(onComplete: (List<PlayerInstrument>?, PlayersErrors?) -> Unit) {
+        if (user.isUserLoggedIn()) {
+            instrumentsDao.instruments(user.getBearerAccessToken()!!) { instrumentsResponse, instrumentsDaoResponseStatus ->
+                if (instrumentsDaoResponseStatus.isSuccessful && instrumentsResponse != null) {
+                    val playerInstruments = instrumentsResponse.map { ir -> toPlayerInstrument(ir) }
+                    onComplete(playerInstruments, null)
+                } else {
+                    onComplete(null, PlayersErrors.UNKNOWN)
+                }
+            }
+        } else {
+            throw ProfileException(ProfileErrors.USER_NOT_LOGGED_IN)
+        }
+    }
+
+    override fun searchInstrument(
+        value: String,
+        onComplete: (List<PlayerInstrument>?, PlayersErrors?) -> Unit
+    ) {
+        if (user.isUserLoggedIn()) {
+            instrumentsDao.search(value, user.getBearerAccessToken()!!) { instrumentsResponse, instrumentsDaoResponseStatus ->
+                if (instrumentsDaoResponseStatus.isSuccessful && instrumentsResponse != null) {
+                    val playerInstruments = instrumentsResponse.map { ir -> toPlayerInstrument(ir) }
+                    onComplete(playerInstruments, null)
+                } else {
+                    onComplete(null, PlayersErrors.UNKNOWN)
+                }
+            }
+        } else {
+            throw ProfileException(ProfileErrors.USER_NOT_LOGGED_IN)
+        }
+    }
+
     private fun toPlayersPage(playersResponse: PlayersResponse) : PlayersPage {
         var players: List<Player> = listOf()
         var isLastPage: Boolean = false
@@ -168,6 +236,20 @@ class PlayersRepositoryImpl(
         }
 
         return PlayersPage(players, isLastPage)
+    }
+
+    private fun toPlayerCity(cityResponse: CityResponse): PlayerCity {
+        return PlayerCity(
+            id = cityResponse.id,
+            name = cityResponse.name
+        )
+    }
+
+    private fun toPlayerInstrument(instrumentResponse: InstrumentResponse): PlayerInstrument {
+        return PlayerInstrument(
+            id = instrumentResponse.id,
+            name = instrumentResponse.name
+        )
     }
 
     override fun loginCompleted(isSuccessful: Boolean, errors: AuthenticationErrors?) {
