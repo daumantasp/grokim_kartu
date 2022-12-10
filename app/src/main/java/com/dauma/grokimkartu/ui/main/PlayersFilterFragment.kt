336cafa01c4fc261e499351bd289008d9ec16890
daumantasp <daumantasp@gmail.com>
2022-12-10 17:19:46 +0200

    Show cities and instruments bottom dialogs on input clicks in players filter view

--- app/src/main/java/com/dauma/grokimkartu/ui/main/PlayersFilterFragment.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/main/PlayersFilterFragment.kt
@@ -1,5 +1,6 @@
 package com.dauma.grokimkartu.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,12 @@ import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.dauma.grokimkartu.R
 import com.dauma.grokimkartu.databinding.FragmentPlayersFilterBinding
 import com.dauma.grokimkartu.general.event.EventObserver
 import com.dauma.grokimkartu.general.utils.Utils
+import com.dauma.grokimkartu.ui.BottomDialogCodeValueData
+import com.dauma.grokimkartu.ui.DialogsManager
 import com.dauma.grokimkartu.viewmodels.main.PlayersFilterViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -18,6 +22,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class PlayersFilterFragment : Fragment() {
     private val playersFilterViewModel by viewModels<PlayersFilterViewModel>()
+    private var dialogsManager: DialogsManager? = null
+    private var isDialogShown: Boolean = false
     @Inject lateinit var utils: Utils
 
     private var _binding: FragmentPlayersFilterBinding? = null
@@ -29,6 +35,11 @@ class PlayersFilterFragment : Fragment() {
         private var TAG = "PlayersFilterFragment"
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        dialogsManager = context as? DialogsManager
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,14 +48,7 @@ class PlayersFilterFragment : Fragment() {
         binding.model = playersFilterViewModel
         val view = binding.root
         setupObservers()
-
-        binding.playersFilterHeaderViewElement.setOnBackClick {
-            playersFilterViewModel.backClicked()
-        }
-
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            playersFilterViewModel.backClicked()
-        }
+        setupOnClickListeners()
 
         playersFilterViewModel.viewIsReady()
         return view
@@ -55,9 +59,101 @@ class PlayersFilterFragment : Fragment() {
         _binding = null
     }
 
+    override fun onDetach() {
+        super.onDetach()
+        dialogsManager = null
+    }
+
     private fun setupObservers() {
         playersFilterViewModel.navigateBack.observe(viewLifecycleOwner, EventObserver {
-            this.findNavController().popBackStack()
+            if (isDialogShown == true) {
+                dialogsManager?.hideBottomDialog()
+                isDialogShown = false
+            } else {
+                this.findNavController().popBackStack()
+            }
         })
+        playersFilterViewModel.city.observe(viewLifecycleOwner, EventObserver { codeValues ->
+            this.isDialogShown = true
+            this.dialogsManager?.let { manager ->
+                val pickableCitiesAsCodeValues = playersFilterViewModel
+                    .getPlayersFilterForm()
+                    .filteredPickableCities
+                    .map { pc -> pc.toCodeValue() }
+
+                manager.showBottomCodeValueDialog(BottomDialogCodeValueData(
+                    title = getString(R.string.profile_edit_city),
+                    codeValues = pickableCitiesAsCodeValues,
+                    onSearchValueChanged = { value ->
+                        this.playersFilterViewModel.searchCity(value) {
+                            val pickableCitiesAsCodeValues = playersFilterViewModel
+                                .getPlayersFilterForm()
+                                .filteredPickableCities
+                                .map { pc -> pc.toCodeValue() }
+                            manager.setCodeValues(pickableCitiesAsCodeValues)
+                        }
+                    },
+                    onCodeValueClicked = { code ->
+                        val id = code.toIntOrNull()
+                        if (id != null) {
+//                            this.playersFilterViewModel.citySelected(id)
+                            manager.hideBottomDialog()
+                            this.isDialogShown = false
+                        }
+                    },
+                    onCancelClicked = {}
+                ))
+            }
+        })
+        playersFilterViewModel.instrument.observe(viewLifecycleOwner, EventObserver { codeValues ->
+            this.isDialogShown = true
+            this.dialogsManager?.let { manager ->
+                val pickableInstrumentsAsCodeValues = playersFilterViewModel
+                    .getPlayersFilterForm()
+                    .filteredPickableInstruments
+                    .map { pi -> pi.toCodeValue() }
+
+                manager.showBottomCodeValueDialog(BottomDialogCodeValueData(
+                    title = getString(R.string.profile_edit_instrument),
+                    codeValues = pickableInstrumentsAsCodeValues,
+                    onSearchValueChanged = { value ->
+                        this.playersFilterViewModel.searchInstrument(value) {
+                            val pickableInstrumentsAsCodeValues = playersFilterViewModel
+                                .getPlayersFilterForm()
+                                .filteredPickableInstruments
+                                .map { pi -> pi.toCodeValue() }
+                            manager.setCodeValues(pickableInstrumentsAsCodeValues)
+                        }
+                    },
+                    onCodeValueClicked = { code ->
+                        val id = code.toIntOrNull()
+                        if (id != null) {
+//                            this.playersFilterViewModel.instrumentSelected(id)
+                            manager.hideBottomDialog()
+                            this.isDialogShown = false
+                        }
+                    },
+                    onCancelClicked = {}
+                ))
+            }
+        })
+    }
+
+    private fun setupOnClickListeners() {
+        binding.playersFilterHeaderViewElement.setOnBackClick {
+            playersFilterViewModel.backClicked()
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            playersFilterViewModel.backClicked()
+        }
+
+        binding.cityInputEditText.setOnClickListener {
+            playersFilterViewModel.cityClicked()
+        }
+
+        binding.instrumentInputEditText.setOnClickListener {
+            playersFilterViewModel.instrumentClicked()
+        }
     }
 }
--- app/src/main/java/com/dauma/grokimkartu/viewmodels/main/PlayersFilterViewModel.kt
+++ app/src/main/java/com/dauma/grokimkartu/viewmodels/main/PlayersFilterViewModel.kt
@@ -15,7 +15,11 @@ class PlayersFilterViewModel @Inject constructor(
     private val playersFilterForm: PlayersFilterForm
 ) : ViewModel() {
     private val _navigateBack = MutableLiveData<Event<String>>()
+    private val _city = MutableLiveData<Event<String>>()
+    private val _instrument = MutableLiveData<Event<String>>()
     val navigateBack: LiveData<Event<String>> = _navigateBack
+    val city: LiveData<Event<String>> = _city
+    val instrument: LiveData<Event<String>> = _instrument
 
     companion object {
         private val TAG = "PlayersFilterViewModel"
@@ -26,9 +30,67 @@ class PlayersFilterViewModel @Inject constructor(
     }
 
     fun viewIsReady() {
+        loadPickableCities()
+        loadPickableInstruments()
     }
 
     fun backClicked() {
         _navigateBack.value = Event("")
+    }
+
+    fun cityClicked() {
+        playersFilterForm.filteredPickableCities = playersFilterForm.pickableCities
+        _city.value = Event("")
+    }
+
+    fun instrumentClicked() {
+        playersFilterForm.filteredPickableInstruments = playersFilterForm.pickableInstruments
+        _instrument.value = Event("")
+    }
+
+    fun searchCity(value: String, onComplete: () -> Unit) {
+        if (value.length > 2) {
+            playersRepository.searchCity(value) { citiesResponse, profileErrors ->
+                if (citiesResponse != null) {
+                    playersFilterForm.filteredPickableCities = citiesResponse
+                }
+                onComplete()
+            }
+        } else {
+            playersFilterForm.filteredPickableCities = playersFilterForm.pickableCities
+            onComplete()
+        }
+    }
+
+    fun searchInstrument(value: String, onComplete: () -> Unit) {
+        if (value.length > 2) {
+            playersRepository.searchInstrument(value) { instrumentsResponse, profileErrors ->
+                if (instrumentsResponse != null) {
+                    playersFilterForm.filteredPickableInstruments = instrumentsResponse
+                }
+                onComplete()
+            }
+        } else {
+            playersFilterForm.filteredPickableInstruments = playersFilterForm.pickableInstruments
+            onComplete()
+        }
+    }
+
+    private fun loadPickableCities() {
+        playersRepository.cities { citiesResponse, profileErrors ->
+            if (citiesResponse != null) {
+                playersFilterForm.pickableCities = citiesResponse
+                playersFilterForm.filteredPickableCities = citiesResponse
+            }
+        }
+    }
+
+    private fun loadPickableInstruments() {
+        playersRepository.instruments { instrumentsResponse, profileErrors ->
+            if (instrumentsResponse != null) {
+                playersFilterForm.pickableInstruments = instrumentsResponse
+                playersFilterForm.filteredPickableInstruments = instrumentsResponse
+            }
+        }
     }
 }
