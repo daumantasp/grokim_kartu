068b052b5117ce219a8c6e9c96fec0c9a71f5dee
daumantasp <daumantasp@gmail.com>
2023-03-01 20:10:56 +0200

    Refactor CitiesDao to use suspend functions

--- app/src/main/java/com/dauma/grokimkartu/data/cities/CitiesDao.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/cities/CitiesDao.kt
@@ -1,8 +1,9 @@
 package com.dauma.grokimkartu.data.cities
 
+import com.dauma.grokimkartu.data.DaoResult
 import com.dauma.grokimkartu.data.cities.entities.CityResponse
 
 interface CitiesDao {
-    fun cities(accessToken: String, onComplete: (List<CityResponse>?, CityDaoResponseStatus) -> Unit)
-    fun search(value: String, accessToken: String, onComplete: (List<CityResponse>?, CityDaoResponseStatus) -> Unit)
+    suspend fun cities(accessToken: String): DaoResult<List<CityResponse>?, CityDaoResponseStatus>
+    suspend fun search(value: String, accessToken: String): DaoResult<List<CityResponse>?, CityDaoResponseStatus>
 }
--- app/src/main/java/com/dauma/grokimkartu/data/cities/CitiesDaoImpl.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/cities/CitiesDaoImpl.kt
@@ -1,79 +1,63 @@
 package com.dauma.grokimkartu.data.cities
 
+import com.dauma.grokimkartu.data.DaoResult
 import com.dauma.grokimkartu.data.cities.entities.CityResponse
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 class CitiesDaoImpl(retrofit: Retrofit) : CitiesDao {
     private val retrofitCities: RetrofitCities = retrofit.create(RetrofitCities::class.java)
 
-    override fun cities(
-        accessToken: String,
-        onComplete: (List<CityResponse>?, CityDaoResponseStatus) -> Unit
-    ) {
-        retrofitCities.cities(accessToken).enqueue(object : Callback<ArrayList<CityResponse>> {
-            override fun onResponse(
-                call: Call<ArrayList<CityResponse>>,
-                response: Response<ArrayList<CityResponse>>
-            ) {
-                when (response.code()) {
-                    200 -> {
-                        val citiesResponse = response.body()
-                        val status = CityDaoResponseStatus(true, null)
-                        onComplete(citiesResponse, status)
-                    }
-                    else -> {
-                        val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(null, status)
-                    }
+    override suspend fun cities(accessToken: String): DaoResult<List<CityResponse>?, CityDaoResponseStatus> {
+        val response = retrofitCities.cities(accessToken)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val citiesResponse = response.body()
+                    val status = CityDaoResponseStatus(true, null)
+                    return DaoResult(citiesResponse, status)
+                }
+                else -> {
+                    val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
                 }
             }
-
-            override fun onFailure(call: Call<ArrayList<CityResponse>>, t: Throwable) {
-                val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(null, status)
-            }
-        })
+        } else {
+            val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
-    override fun search(
+    override suspend fun search(
         value: String,
-        accessToken: String,
-        onComplete: (List<CityResponse>?, CityDaoResponseStatus) -> Unit
-    ) {
-        retrofitCities.search(accessToken, value).enqueue(object : Callback<ArrayList<CityResponse>> {
-            override fun onResponse(
-                call: Call<ArrayList<CityResponse>>,
-                response: Response<ArrayList<CityResponse>>
-            ) {
-                when (response.code()) {
-                    200 -> {
-                        val citiesResponse = response.body()
-                        val status = CityDaoResponseStatus(true, null)
-                        onComplete(citiesResponse, status)
-                    }
-                    else -> {
-                        val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(null, status)
-                    }
+        accessToken: String
+    ) : DaoResult<List<CityResponse>?, CityDaoResponseStatus> {
+        val response = retrofitCities.search(accessToken, value)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val citiesResponse = response.body()
+                    val status = CityDaoResponseStatus(true, null)
+                    return DaoResult(citiesResponse, status)
+                }
+                else -> {
+                    val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
                 }
             }
-
-            override fun onFailure(call: Call<ArrayList<CityResponse>>, t: Throwable) {
-                val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(null, status)
-            }
-        })
+        } else {
+            val status = CityDaoResponseStatus(false, CityDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
     private interface RetrofitCities {
-        @GET("cities") fun cities(@Header("Authorization") accessToken: String): Call<ArrayList<CityResponse>>
-        @GET("cities/search") fun search(@Header("Authorization") accessToken: String, @Query("value") value: String): Call<ArrayList<CityResponse>>
+        @GET("cities") suspend fun cities(@Header("Authorization") accessToken: String): Response<ArrayList<CityResponse>>
+        @GET("cities/search") suspend fun search(@Header("Authorization") accessToken: String, @Query("value") value: String): Response<ArrayList<CityResponse>>
     }
 }
