14251bd647bceef7744930e46818d643ec85bee7
daumantasp <daumantasp@gmail.com>
2023-03-01 20:29:11 +0200

    Refactor SettingsDao to use suspend functions

--- app/src/main/java/com/dauma/grokimkartu/data/settings/SettingsDao.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/settings/SettingsDao.kt
@@ -1,13 +1,14 @@
 package com.dauma.grokimkartu.data.settings
 
+import com.dauma.grokimkartu.data.DaoResult
 import com.dauma.grokimkartu.data.settings.entities.DeletePushNotificationsTokenRequest
 import com.dauma.grokimkartu.data.settings.entities.PushNotificationsTokenRequest
 import com.dauma.grokimkartu.data.settings.entities.SettingsResponse
 import com.dauma.grokimkartu.data.settings.entities.UpdateSettingsRequest
 
 interface SettingsDao {
-    fun settings(accessToken: String, onComplete: (SettingsResponse?, SettingsDaoResponseStatus) -> Unit)
-    fun update(updateSettingsRequest: UpdateSettingsRequest, accessToken: String, onComplete: (SettingsResponse?, SettingsDaoResponseStatus) -> Unit)
-    fun pushNotificationsToken(pushNotificationsTokenRequest: PushNotificationsTokenRequest, accessToken: String, onComplete: (SettingsDaoResponseStatus) -> Unit)
-    fun deletePushNotificationsToken(deletePushNotificationsTokenRequest: DeletePushNotificationsTokenRequest, accessToken: String, onComplete: (SettingsDaoResponseStatus) -> Unit)
+    suspend fun settings(accessToken: String): DaoResult<SettingsResponse?, SettingsDaoResponseStatus>
+    suspend fun update(updateSettingsRequest: UpdateSettingsRequest, accessToken: String): DaoResult<SettingsResponse?, SettingsDaoResponseStatus>
+    suspend fun pushNotificationsToken(pushNotificationsTokenRequest: PushNotificationsTokenRequest, accessToken: String): DaoResult<Nothing?, SettingsDaoResponseStatus>
+    suspend fun deletePushNotificationsToken(deletePushNotificationsTokenRequest: DeletePushNotificationsTokenRequest, accessToken: String): DaoResult<Nothing?, SettingsDaoResponseStatus>
 }
--- app/src/main/java/com/dauma/grokimkartu/data/settings/SettingsDaoImpl.kt
+++ app/src/main/java/com/dauma/grokimkartu/data/settings/SettingsDaoImpl.kt
@@ -1,11 +1,10 @@
 package com.dauma.grokimkartu.data.settings
 
+import com.dauma.grokimkartu.data.DaoResult
 import com.dauma.grokimkartu.data.settings.entities.DeletePushNotificationsTokenRequest
 import com.dauma.grokimkartu.data.settings.entities.PushNotificationsTokenRequest
 import com.dauma.grokimkartu.data.settings.entities.SettingsResponse
 import com.dauma.grokimkartu.data.settings.entities.UpdateSettingsRequest
-import retrofit2.Call
-import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.*
@@ -13,139 +12,122 @@ import retrofit2.http.*
 class SettingsDaoImpl(retrofit: Retrofit): SettingsDao {
     private val retrofitSettings: RetrofitSettings = retrofit.create(RetrofitSettings::class.java)
 
-    override fun settings(
-        accessToken: String,
-        onComplete: (SettingsResponse?, SettingsDaoResponseStatus) -> Unit
-    ) {
-        retrofitSettings.settings(accessToken).enqueue(object : Callback<SettingsResponse> {
-            override fun onResponse(
-                call: Call<SettingsResponse>,
-                response: Response<SettingsResponse>
-            ) {
-                when (response.code()) {
-                    200 -> {
-                        val settingsResponseData = response.body()
-                        val status = SettingsDaoResponseStatus(true, null)
-                        onComplete(settingsResponseData, status)
-                    }
-                    else -> {
-                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(null, status)
-                    }
+    override suspend fun settings(accessToken: String): DaoResult<SettingsResponse?, SettingsDaoResponseStatus> {
+        val response = retrofitSettings.settings(accessToken)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val settingsResponseData = response.body()
+                    val status = SettingsDaoResponseStatus(true, null)
+                    return DaoResult(settingsResponseData, status)
+                }
+                else -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
                 }
             }
-
-            override fun onFailure(call: Call<SettingsResponse>, t: Throwable) {
-                val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(null, status)
-            }
-        })
+        } else {
+            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
-    override fun update(
+    override suspend fun update(
         updateSettingsRequest: UpdateSettingsRequest,
-        accessToken: String,
-        onComplete: (SettingsResponse?, SettingsDaoResponseStatus) -> Unit
-    ) {
-        retrofitSettings.update(accessToken, updateSettingsRequest).enqueue(object : Callback<SettingsResponse> {
-            override fun onResponse(
-                call: Call<SettingsResponse>,
-                response: Response<SettingsResponse>
-            ) {
-                when (response.code()) {
-                    200 -> {
-                        val settingsResponseData = response.body()
-                        val status = SettingsDaoResponseStatus(true, null)
-                        onComplete(settingsResponseData, status)
-                    }
-                    500 -> {
-                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.PROFILE_NOT_FOUND)
-                        onComplete(null, status)
-                    }
-                    else -> {
-                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(null, status)
-                    }
+        accessToken: String
+    ): DaoResult<SettingsResponse?, SettingsDaoResponseStatus> {
+        val response = retrofitSettings.update(accessToken, updateSettingsRequest)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val settingsResponseData = response.body()
+                    val status = SettingsDaoResponseStatus(true, null)
+                    return DaoResult(settingsResponseData, status)
+                }
+                500 -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.PROFILE_NOT_FOUND)
+                    return DaoResult(null, status)
+                }
+                else -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
                 }
             }
-
-            override fun onFailure(call: Call<SettingsResponse>, t: Throwable) {
-                val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(null, status)
-            }
-        })
+        } else {
+            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
-    override fun pushNotificationsToken(
+    override suspend fun pushNotificationsToken(
         pushNotificationsTokenRequest: PushNotificationsTokenRequest,
-        accessToken: String,
-        onComplete: (SettingsDaoResponseStatus) -> Unit
-    ) {
-        retrofitSettings.token(accessToken, pushNotificationsTokenRequest).enqueue(object : Callback<Array<String>> {
-            override fun onResponse(call: Call<Array<String>>, response: Response<Array<String>>) {
-                when (response.code()) {
-                    200 -> {
-                        val status = SettingsDaoResponseStatus(true, null)
-                        onComplete(status)
-                    }
-                    400 -> {
-                        val errorBody = response.errorBody()?.string() ?: ""
-                        if (errorBody.contains(SettingsDaoResponseStatus.Errors.TOKEN_IS_NOT_PROVIDED.toString(), true)) {
-                            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.TOKEN_IS_NOT_PROVIDED)
-                            onComplete(status)
-                        } else if (errorBody.contains(SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED.toString(), true)) {
-                            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED)
-                            onComplete(status)
-                        }
-                    }
-                    else -> {
+        accessToken: String
+    ): DaoResult<Nothing?, SettingsDaoResponseStatus> {
+        val response = retrofitSettings.token(accessToken, pushNotificationsTokenRequest)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val status = SettingsDaoResponseStatus(true, null)
+                    return DaoResult(null, status)
+                }
+                400 -> {
+                    val errorBody = response.errorBody()?.string() ?: ""
+                    if (errorBody.contains(SettingsDaoResponseStatus.Errors.TOKEN_IS_NOT_PROVIDED.toString(), true)) {
+                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.TOKEN_IS_NOT_PROVIDED)
+                        return DaoResult(null, status)
+                    } else if (errorBody.contains(SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED.toString(), true)) {
+                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED)
+                        return DaoResult(null, status)
+                    } else {
                         val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(status)
+                        return DaoResult(null, status)
                     }
                 }
+                else -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
+                }
             }
-
-            override fun onFailure(call: Call<Array<String>>, t: Throwable) {
-                val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(status)
-            }
-        })
+        } else {
+            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
-    override fun deletePushNotificationsToken(
+    override suspend fun deletePushNotificationsToken(
         deletePushNotificationsTokenRequest: DeletePushNotificationsTokenRequest,
-        accessToken: String,
-        onComplete: (SettingsDaoResponseStatus) -> Unit
-    ) {
-        retrofitSettings.token(accessToken, deletePushNotificationsTokenRequest).enqueue(object : Callback<Array<String>> {
-            override fun onResponse(call: Call<Array<String>>, response: Response<Array<String>>) {
-                when (response.code()) {
-                    200 -> {
-                        val status = SettingsDaoResponseStatus(true, null)
-                        onComplete(status)
-                    }
-                    400 -> {
-                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED)
-                        onComplete(status)
-                    }
-                    else -> {
-                        val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                        onComplete(status)
-                    }
+        accessToken: String
+    ): DaoResult<Nothing?, SettingsDaoResponseStatus> {
+        val response = retrofitSettings.token(accessToken, deletePushNotificationsTokenRequest)
+
+        if (response.isSuccessful) {
+            when (response.code()) {
+                200 -> {
+                    val status = SettingsDaoResponseStatus(true, null)
+                    return DaoResult(null, status)
+                }
+                400 -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.DEVICE_ID_IS_NOT_PROVIDED)
+                    return DaoResult(null, status)
+                }
+                else -> {
+                    val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+                    return DaoResult(null, status)
                 }
             }
-
-            override fun onFailure(call: Call<Array<String>>, t: Throwable) {
-                val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
-                onComplete(status)
-            }
-        })
+        } else {
+            val status = SettingsDaoResponseStatus(false, SettingsDaoResponseStatus.Errors.UNKNOWN)
+            return DaoResult(null, status)
+        }
     }
 
     private interface RetrofitSettings {
-        @GET("settings") fun settings(@Header("Authorization") accessToken: String): Call<SettingsResponse>
-        @PUT("settings") fun update(@Header("Authorization") accessToken: String, @Body updateRequest: UpdateSettingsRequest): Call<SettingsResponse>
-        @PUT("settings/push-notifications/token") fun token(@Header("Authorization") accessToken: String, @Body tokenRequest: PushNotificationsTokenRequest): Call<Array<String>>
-        @DELETE("settings/push-notifications/token") fun token(@Header("Authorization") accessToken: String, @Body tokenRequest: DeletePushNotificationsTokenRequest): Call<Array<String>>
+        @GET("settings") suspend fun settings(@Header("Authorization") accessToken: String): Response<SettingsResponse>
+        @PUT("settings") suspend fun update(@Header("Authorization") accessToken: String, @Body updateRequest: UpdateSettingsRequest): Response<SettingsResponse>
+        @PUT("settings/push-notifications/token") suspend fun token(@Header("Authorization") accessToken: String, @Body tokenRequest: PushNotificationsTokenRequest): Response<Array<String>>
+        @DELETE("settings/push-notifications/token") suspend fun token(@Header("Authorization") accessToken: String, @Body tokenRequest: DeletePushNotificationsTokenRequest): Response<Array<String>>
     }
 }
