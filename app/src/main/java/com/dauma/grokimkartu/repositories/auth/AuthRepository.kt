871ec10375ca811cf8cbb4921fb1bd71a5fa8081
daumantasp <daumantasp@gmail.com>
2023-03-05 14:03:10 +0200

    Refactor AuthRepository to more correspond to MVVM pattern

--- app/src/main/java/com/dauma/grokimkartu/repositories/auth/AuthRepository.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/auth/AuthRepository.kt
@@ -1,14 +1,15 @@
 package com.dauma.grokimkartu.repositories.auth
 
 import com.dauma.grokimkartu.repositories.users.AuthenticationErrors
+import com.dauma.grokimkartu.repositories.Result
 
 interface AuthRepository {
-    fun register(email: String, password: String, name: String, onComplete: (Boolean?, AuthenticationErrors?) -> Unit)
-    fun login(email: String, password: String)
-    fun tryReauthenticate()
-    fun logout()
-    fun delete(onComplete: (Boolean, AuthenticationErrors?) -> Unit)
-    fun changePassword(oldPassword: String, newPassword: String, newPasswordConfirmed: String, onComplete: (Boolean, AuthenticationErrors?) -> Unit)
+    suspend fun register(email: String, password: String, name: String): Result<Boolean?, AuthenticationErrors?>
+    suspend fun login(email: String, password: String)
+    suspend fun tryReauthenticate()
+    suspend fun logout()
+    suspend fun delete(): Result<Boolean, AuthenticationErrors?>
+    suspend fun changePassword(oldPassword: String, newPassword: String, newPasswordConfirmed: String): Result<Boolean, AuthenticationErrors?>
     fun registerLoginListener(id: String, listener: LoginListener)
     fun unregisterLoginListener(id: String)
     fun registerLogoutListener(id: String, listener: LogoutListener)
--- app/src/main/java/com/dauma/grokimkartu/repositories/auth/AuthRepositoryImpl.kt
+++ app/src/main/java/com/dauma/grokimkartu/repositories/auth/AuthRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.dauma.grokimkartu.data.auth.AuthDaoResponseStatus
 import com.dauma.grokimkartu.data.auth.entities.*
 import com.dauma.grokimkartu.general.user.User
 import com.dauma.grokimkartu.general.utils.Utils
+import com.dauma.grokimkartu.repositories.Result
 import com.dauma.grokimkartu.repositories.auth.AuthRepository
 import com.dauma.grokimkartu.repositories.auth.LoginListener
 import com.dauma.grokimkartu.repositories.auth.LogoutListener
@@ -21,12 +22,11 @@ class AuthRepositoryImpl(
         private const val USER_ACCESS_TOKEN_KEY = "USER_ACCESS_TOKEN_KEY"
     }
 
-    override fun register(
+    override suspend fun register(
         email: String,
         password: String,
-        name: String,
-        onComplete: (Boolean?, AuthenticationErrors?) -> Unit
-    ) {
+        name: String
+    ): Result<Boolean?, AuthenticationErrors?> {
         if (user.isUserLoggedIn() == false) {
             val registrationRequest = RegistrationRequest(
                 name = name,
@@ -34,20 +34,21 @@ class AuthRepositoryImpl(
                 password = password,
                 passwordConfirmation = password
             )
-            authDao.register(registrationRequest) { registrationResponse, authDaoResponseStatus ->
-                if (authDaoResponseStatus.isSuccessful && registrationResponse != null) {
-                    onComplete(true, null)
-                } else {
-                    when (authDaoResponseStatus.error) {
-                        AuthDaoResponseStatus.Errors.EMAIL_TAKEN -> {
-                            onComplete(false, AuthenticationErrors.EMAIL_TAKEN)
-                        }
-                        AuthDaoResponseStatus.Errors.INVALID_EMAIL -> {
-                            onComplete(false, AuthenticationErrors.INVALID_EMAIL)
-                        }
-                        else -> {
-                            onComplete(false, AuthenticationErrors.UNKNOWN)
-                        }
+            val response = authDao.register(registrationRequest)
+            val status = response.status
+            val registrationResponse = response.data
+            if (status.isSuccessful && registrationResponse != null) {
+                return Result(true, null)
+            } else {
+                when (status.error) {
+                    AuthDaoResponseStatus.Errors.EMAIL_TAKEN -> {
+                        return Result(false, AuthenticationErrors.EMAIL_TAKEN)
+                    }
+                    AuthDaoResponseStatus.Errors.INVALID_EMAIL -> {
+                        return Result(false, AuthenticationErrors.INVALID_EMAIL)
+                    }
+                    else -> {
+                        return Result(false, AuthenticationErrors.UNKNOWN)
                     }
                 }
             }
@@ -56,58 +57,60 @@ class AuthRepositoryImpl(
         }
     }
 
-    override fun login(
+    override suspend fun login(
         email: String,
         password: String
     ) {
         if (user.isUserLoggedIn() == false) {
             val loginRequest = LoginRequest(email, password)
-            authDao.login(loginRequest) { loginResponse, authDaoResponseStatus ->
-                if (authDaoResponseStatus.isSuccessful == true && loginResponse != null) {
+            val response = authDao.login(loginRequest)
+            val status = response.status
+            val loginResponse = response.data
+            if (status.isSuccessful && loginResponse != null) {
+                user.login(loginResponse)
+                if (loginResponse.accessToken != null) {
+                    saveAccessTokenToStorage(loginResponse.accessToken!!)
+                }
+                notifyLoginListeners(true, null)
+            } else {
+                when (status.error) {
+                    AuthDaoResponseStatus.Errors.INCORRECT_USR_NAME_OR_PSW -> {
+                        notifyLoginListeners(false, AuthenticationErrors.INCORRECT_USR_NAME_OR_PSW)
+                    }
+                    AuthDaoResponseStatus.Errors.EMAIL_NOT_VERIFIED -> {
+                        notifyLoginListeners(false, AuthenticationErrors.EMAIL_NOT_VERIFIED)
+                    }
+                    else -> {
+                        notifyLoginListeners(false, AuthenticationErrors.UNKNOWN)
+                    }
+                }
+            }
+        } else {
+            throw AuthenticationException(AuthenticationErrors.USER_ALREADY_LOGGED_IN)
+        }
+    }
+
+    override suspend fun tryReauthenticate() {
+        if (user.isUserLoggedIn() == false) {
+            val accessToken = getAccessTokenFromStorage()
+            if (accessToken != null) {
+                val reauthenticateRequest = ReauthenticateRequest(accessToken)
+                val response = authDao.reauthenticate(reauthenticateRequest)
+                val status = response.status
+                val loginResponse = response.data
+                if (status.isSuccessful && loginResponse != null) {
                     user.login(loginResponse)
                     if (loginResponse.accessToken != null) {
                         saveAccessTokenToStorage(loginResponse.accessToken!!)
                     }
                     notifyLoginListeners(true, null)
                 } else {
-                    when (authDaoResponseStatus.error) {
-                        AuthDaoResponseStatus.Errors.INCORRECT_USR_NAME_OR_PSW -> {
-                            notifyLoginListeners(false, AuthenticationErrors.INCORRECT_USR_NAME_OR_PSW)
-                        }
-                        AuthDaoResponseStatus.Errors.EMAIL_NOT_VERIFIED -> {
-                            notifyLoginListeners(false, AuthenticationErrors.EMAIL_NOT_VERIFIED)
+                    when (status.error) {
+                        AuthDaoResponseStatus.Errors.INCORRECT_ACCESS_TOKEN -> {
+                            notifyLoginListeners(false, AuthenticationErrors.INCORRECT_ACCESS_TOKEN)
                         }
                         else -> {
                             notifyLoginListeners(false, AuthenticationErrors.UNKNOWN)
-                        }
-                    }
-                }
-            }
-        } else {
-            throw AuthenticationException(AuthenticationErrors.USER_ALREADY_LOGGED_IN)
-        }
-    }
-
-    override fun tryReauthenticate() {
-        if (user.isUserLoggedIn() == false) {
-            val accessToken = getAccessTokenFromStorage()
-            if (accessToken != null) {
-                val reauthenticateRequest = ReauthenticateRequest(accessToken)
-                authDao.reauthenticate(reauthenticateRequest) { loginResponse, authDaoResponseStatus ->
-                    if (authDaoResponseStatus.isSuccessful == true && loginResponse != null) {
-                        user.login(loginResponse)
-                        if (loginResponse.accessToken != null) {
-                            saveAccessTokenToStorage(loginResponse.accessToken!!)
-                        }
-                        this.notifyLoginListeners(true, null)
-                    } else {
-                        when (authDaoResponseStatus.error) {
-                            AuthDaoResponseStatus.Errors.INCORRECT_ACCESS_TOKEN -> {
-                                notifyLoginListeners(false, AuthenticationErrors.INCORRECT_ACCESS_TOKEN)
-                            }
-                            else -> {
-                                notifyLoginListeners(false, AuthenticationErrors.UNKNOWN)
-                            }
                         }
                     }
                 }
@@ -122,61 +125,60 @@ class AuthRepositoryImpl(
         }
     }
 
-    override fun logout() {
+    override suspend fun logout() {
         if (user.isUserLoggedIn()) {
             val logoutRequest = LogoutRequest(utils.appUtils.deviceId())
-            authDao.logout(logoutRequest, user.getBearerAccessToken()!!) { authDaoResponseStatus ->
-                if (authDaoResponseStatus.isSuccessful) {
-                    user.logout()
-                    removeAccessTokenFromStorage()
-                    notifyLogoutListeners(true, null)
-                } else {
-                    notifyLogoutListeners(false, AuthenticationErrors.UNKNOWN)
-                }
+            val response = authDao.logout(logoutRequest, user.getBearerAccessToken()!!)
+            val status = response.status
+            if (status.isSuccessful) {
+                user.logout()
+                removeAccessTokenFromStorage()
+                notifyLogoutListeners(true, null)
+            } else {
+                notifyLogoutListeners(false, AuthenticationErrors.UNKNOWN)
             }
         } else {
             throw AuthenticationException(AuthenticationErrors.USER_NOT_LOGGED_IN)
         }
     }
 
-    override fun delete(onComplete: (Boolean, AuthenticationErrors?) -> Unit) {
+    override suspend fun delete(): Result<Boolean, AuthenticationErrors?> {
         if (user.isUserLoggedIn()) {
-            authDao.delete(user.getBearerAccessToken()!!) { authDaoResponseStatus ->
-                if (authDaoResponseStatus.isSuccessful) {
-                    user.logout()
-                    removeAccessTokenFromStorage()
-                    onComplete(true, null)
-                } else {
-                    onComplete(false, AuthenticationErrors.UNKNOWN)
-                }
+            val response = authDao.delete(user.getBearerAccessToken()!!)
+            val status = response.status
+            if (status.isSuccessful) {
+                user.logout()
+                removeAccessTokenFromStorage()
+                return Result(true, null)
+            } else {
+                return Result(false, AuthenticationErrors.UNKNOWN)
             }
         } else {
             throw AuthenticationException(AuthenticationErrors.USER_NOT_LOGGED_IN)
         }
     }
 
-    override fun changePassword(
+    override suspend fun changePassword(
         oldPassword: String,
         newPassword: String,
-        newPasswordConfirmed: String,
-        onComplete: (Boolean, AuthenticationErrors?) -> Unit
-    ) {
+        newPasswordConfirmed: String
+    ): Result<Boolean, AuthenticationErrors?> {
         if (user.isUserLoggedIn()) {
             val changePasswordRequest = ChangePasswordRequest(oldPassword, newPassword, newPasswordConfirmed)
-            authDao.changePassword(user.getBearerAccessToken()!!, changePasswordRequest) { authDaoResponseStatus ->
-                if (authDaoResponseStatus.isSuccessful) {
-                    onComplete(true, null)
-                } else {
-                    when (authDaoResponseStatus.error) {
-                        AuthDaoResponseStatus.Errors.INCORRECT_OLD_PSW -> {
-                            onComplete(false, AuthenticationErrors.INCORRECT_OLD_PSW)
-                        }
-                        AuthDaoResponseStatus.Errors.NEW_PSW_SIMILAR -> {
-                            onComplete(false, AuthenticationErrors.NEW_PSW_SIMILAR)
-                        }
-                        else -> {
-                            onComplete(false, AuthenticationErrors.UNKNOWN)
-                        }
+            val response = authDao.changePassword(user.getBearerAccessToken()!!, changePasswordRequest)
+            val status = response.status
+            if (status.isSuccessful) {
+                return Result(true, null)
+            } else {
+                when (status.error) {
+                    AuthDaoResponseStatus.Errors.INCORRECT_OLD_PSW -> {
+                        return Result(false, AuthenticationErrors.INCORRECT_OLD_PSW)
+                    }
+                    AuthDaoResponseStatus.Errors.NEW_PSW_SIMILAR -> {
+                        return Result(false, AuthenticationErrors.NEW_PSW_SIMILAR)
+                    }
+                    else -> {
+                        return Result(false, AuthenticationErrors.UNKNOWN)
                     }
                 }
             }
