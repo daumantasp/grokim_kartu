f4abbae1bf02bc4170446c41e775d247c28335d5
daumantasp <daumantasp@gmail.com>
2023-11-08 13:44:12 +0200

    Refactor SplashFragmanet and SplashViewModel to correspond more to MVVM pattern

--- app/src/main/java/com/dauma/grokimkartu/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/dauma/grokimkartu/ui/splash/SplashFragment.kt
@@ -1,20 +1,24 @@
 package com.dauma.grokimkartu.ui.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.dauma.grokimkartu.databinding.FragmentSplashBinding
-import com.dauma.grokimkartu.general.event.EventObserver
 import com.dauma.grokimkartu.general.navigationcommand.NavigationCommand
 import com.dauma.grokimkartu.viewmodels.splash.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SplashFragment : Fragment() {
+
     private val splashViewModel by viewModels<SplashViewModel>()
 
     private var _binding: FragmentSplashBinding? = null
@@ -25,30 +29,33 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         binding.model = splashViewModel
         val view = binding.root
         setupObservers()
-        splashViewModel.viewIsReady()
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-        splashViewModel.viewIsDiscarded()
     }
 
     private fun setupObservers() {
-        splashViewModel.navigation.observe(viewLifecycleOwner, EventObserver {
-            handleNavigation(it)
-        })
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                splashViewModel.navigation.collect {
+                    handleNavigation(it)
+                }
+            }
+        }
     }
 
-    private fun handleNavigation(navigationCommand: NavigationCommand) {
+    private fun handleNavigation(navigationCommand: NavigationCommand?) {
         when (navigationCommand) {
             is NavigationCommand.ToDirection -> findNavController().navigate(navigationCommand.directions)
+            else -> {}
         }
     }
 }
--- app/src/main/java/com/dauma/grokimkartu/viewmodels/splash/SplashViewModel.kt
+++ app/src/main/java/com/dauma/grokimkartu/viewmodels/splash/SplashViewModel.kt
@@ -1,44 +1,47 @@
 package com.dauma.grokimkartu.viewmodels.splash
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.dauma.grokimkartu.general.event.Event
+import androidx.lifecycle.viewModelScope
 import com.dauma.grokimkartu.general.navigationcommand.NavigationCommand
 import com.dauma.grokimkartu.repositories.auth.AuthRepository
-import com.dauma.grokimkartu.repositories.auth.LoginListener
-import com.dauma.grokimkartu.repositories.users.AuthenticationErrors
+import com.dauma.grokimkartu.repositories.auth.AuthState
 import com.dauma.grokimkartu.ui.splash.SplashFragmentDirections
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val authRepository: AuthRepository
-) : ViewModel(), LoginListener {
-    private val _navigation = MutableLiveData<Event<NavigationCommand>>()
-    val navigation: LiveData<Event<NavigationCommand>> = _navigation
+) : ViewModel() {
 
-    companion object {
-        private val TAG = "SplashViewModelImpl"
-        private const val SPLASH_VIEW_MODEL_LOGIN_LISTENER_ID = "SPLASH_VIEW_MODEL_LOGIN_LISTENER_ID"
-    }
+    private val _navigation: MutableStateFlow<NavigationCommand?> = MutableStateFlow(null)
+    val navigation: StateFlow<NavigationCommand?> = _navigation
 
-    fun viewIsReady() {
-        authRepository.registerLoginListener(SPLASH_VIEW_MODEL_LOGIN_LISTENER_ID, this)
-//        authRepository.tryReauthenticate()
-    }
-
-    fun viewIsDiscarded() {
-        authRepository.unregisterLoginListener(SPLASH_VIEW_MODEL_LOGIN_LISTENER_ID)
-    }
-
-    override fun loginCompleted(isSuccessful: Boolean, errors: AuthenticationErrors?) {
-        val navDirection = if (isSuccessful) {
-            SplashFragmentDirections.actionSplashFragmentToHomeGraph()
-        } else {
-            SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+    init {
+        viewModelScope.launch {
+            observeAuthState()
         }
-        _navigation.value = Event(NavigationCommand.ToDirection(navDirection))
+        viewModelScope.launch {
+            authRepository.tryReauthenticate()
+        }
+    }
+
+    private suspend fun observeAuthState() {
+        authRepository.authState.collect { authState ->
+            when (authState) {
+                is AuthState.LoginCompleted -> {
+                    val navDirection = if (authState.isSuccessful) {
+                        SplashFragmentDirections.actionSplashFragmentToHomeGraph()
+                    } else {
+                        SplashFragmentDirections.actionSplashFragmentToLoginFragment()
+                    }
+                    _navigation.value = NavigationCommand.ToDirection(navDirection)
+                }
+                else -> {}
+            }
+        }
     }
 }
